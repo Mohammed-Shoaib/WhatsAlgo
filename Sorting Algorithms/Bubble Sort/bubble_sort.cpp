ae00851b15179835a898c3826f0e0d5cddc86588
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-06 20:41:55 +0400

    Add test cases for Bubble Sort

--- Sorting Algorithms/Bubble Sort/bubble_sort.cpp
+++ Sorting Algorithms/Bubble Sort/bubble_sort.cpp
@@ -1,33 +1,16 @@
 /*
 Algorithm: Bubble Sort
-Time Complexity: O(n^2)
-Space Complexity: O(1)
+Time: O(n²)
+Space: O(1)
 Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include "bubble_sort.h"
 
-using namespace std;
-
-void bubble_sort(vector<int> &a) {
-    int n = a.size();
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
-            if (a[j] > a[j + 1])
-                swap(a[j], a[j + 1]);
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    bubble_sort(a);
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
-    cout << endl;
-    return 0;
+void bubble_sort(std::vector<int>& a) {
+	int n = a.size();
+	for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - i - 1; j++)
+			if (a[j] > a[j + 1])
+				std::swap(a[j], a[j + 1]);
 }
---
+++ Sorting Algorithms/Bubble Sort/bubble_sort.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+void bubble_sort(std::vector<int>& a);

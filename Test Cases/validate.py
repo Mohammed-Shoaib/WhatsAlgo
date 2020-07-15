376792efb0c554a4f53fbc214672223c0e3d2191
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-15 07:29:52 +0400

    Add sample test cases for Edit Distance

--- Test Cases/validate.py
+++ Test Cases/validate.py
@@ -108,6 +108,7 @@ if __name__ == '__main__':
 	run_tests('Dynamic Programming/Maximum Subarray')
 	run_tests('Dynamic Programming/Coin Change')
 	run_tests('Dynamic Programming/nCr mod m')
+	run_tests('Dynamic Programming/Edit Distance')
 
 	# Greedy Algorithms
 	run_tests('Greedy Algorithms/Coin Change')
